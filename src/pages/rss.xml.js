c59479a3123601baf83a72d7dac89ee24886d1f9
Hugo Filipe Goncalves <hugo.f.goncalves@noesis.pt>
2026-09-22 01:07:25 +0100

    fix: add button on 404 page

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -8,7 +8,7 @@ export async function GET(context) {
 
   return rss({
     title: 'Hugo Gonçalves',
-    description: 'Blog posts from HG ⴾ',
+    description: 'Blog posts from HGⴾ',
     site: context.site,
     items: posts.map((post) => ({
       title: post.data.title,
